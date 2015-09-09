fb25dc1f76d7f0d7b94c05cc148187204b70fe65
Reio raudheiding <reiorau@greeny.cs.tlu.ee>
2015-09-09 13:22:03 +0300

    t2hes6jad

--- login.php
+++ login.php
@@ -3,6 +3,7 @@
 	// LOGIN.PHP
 	//echo $_POST["email"];
 	$email_error = "";
+	$password_error = "";
 	
 	//kontrollime kas keegi vajutas input nuppu
 	if($_SERVER["REQUEST_METHOD"] == "POST") {
@@ -13,6 +14,18 @@
 		if ( empty($_POST["email"])) {
 			$email_error = "See väli on kohustuslik";
 		}
+		
+		//kontrollin et parool ei ole tühi
+		if ( empty($_POST["password"])) {
+			$password_error = "See väli on kohustuslik";
+		} else {
+			//kui oleme siia jõudnud, siis parool ei ole tühi
+			//kontrollin, et oleks vähemalt 8 sümbolit pikk
+			if(strlen($_POST["password"]) < 8) {
+				$password_error = "Peab olema vähemalt 8 tähemärki pikk.";
+			}
+		}
+		
 	}
 ?>
 <html>
@@ -24,7 +37,7 @@
 	
 		<form action="login.php" method="post" >
 		<input name="email" type="email" placeholder="E-post"> <?php echo $email_error; ?> <br><br>
-		<input name="password" type="password" placeholder="parool"> <br><br>
+		<input name="password" type="password" placeholder="parool"> <?php echo $password_error; ?> <br><br>
 		<input type="submit" value="Log in"> <br><br>
 		</form>
 	<h2>Create user</h2>
