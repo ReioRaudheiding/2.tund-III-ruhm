b1ec551150f3832ded161785d0b8be31a768f2ee
Reio raudheiding <reiorau@greeny.cs.tlu.ee>
2015-09-09 13:08:49 +0300

    master login

--- login.php
+++ login.php
@@ -1,8 +1,19 @@
 <?php
 	
 	// LOGIN.PHP
+	//echo $_POST["email"];
+	$email_error = "";
 	
-	
+	//kontrollime kas keegi vajutas input nuppu
+	if($_SERVER["REQUEST_METHOD"] == "POST") {
+		
+		//echo "keegi vajutas nuppu";
+		
+		//kontrollin et e-post ei ole tühi
+		if ( empty($_POST["email"])) {
+			$email_error = "See väli on kohustuslik";
+		}
+	}
 ?>
 <html>
 <head>
@@ -10,9 +21,12 @@
 </head>
 <body>
 	<h2>Log in</h2>
-		<input type="email" placeholder="E-post"> <br><br>
-		<input type="password" placeholder="parool"> <br><br>
+	
+		<form action="login.php" method="post" >
+		<input name="email" type="email" placeholder="E-post"> <?php echo $email_error; ?> <br><br>
+		<input name="password" type="password" placeholder="parool"> <br><br>
 		<input type="submit" value="Log in"> <br><br>
+		</form>
 	<h2>Create user</h2>
 </body>
 </html>
