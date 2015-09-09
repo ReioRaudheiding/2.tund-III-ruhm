a8f1a0586e4229dfbf2de861cef4f227652781cc
Reio raudheiding <reiorau@greeny.cs.tlu.ee>
2015-09-09 12:38:33 +0300

    logins made

---
+++ login.php
@@ -0,0 +1,18 @@
+<?php
+	
+	// LOGIN.PHP
+	
+	
+?>
+<html>
+<head>
+	<title>Login Page</title>
+</head>
+<body>
+	<h2>Log in</h2>
+		<input type="email" placeholder="E-post"> <br><br>
+		<input type="password" placeholder="parool"> <br><br>
+		<input type="submit" value="Log in"> <br><br>
+	<h2>Create user</h2>
+</body>
+</html>
